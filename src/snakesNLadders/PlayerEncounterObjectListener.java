f222803290aea57de221d484c70a2625aea15c1a
AkhilDiddiga <47138605+AkhilDiddiga@users.noreply.github.com>
2019-01-29 17:54:54 +0530

    Update PlayerEncounterObjectListener.java

--- src/snakesNLadders/PlayerEncounterObjectListener.java
+++ src/snakesNLadders/PlayerEncounterObjectListener.java
@@ -1,5 +1,6 @@
 package snakesNLadders;
 
+// this listener is invoked when any player encounters a snake or a ladder
 public interface PlayerEncounterObjectListener {
 	void playerEncounterSnake(Player P);
 	void playerEncounterLadder(Player P);
