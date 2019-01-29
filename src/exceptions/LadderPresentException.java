8b7fac7ab49ceb69afcbe3f0bcfea52179d21dad
AkhilDiddiga <47138605+AkhilDiddiga@users.noreply.github.com>
2019-01-29 17:46:02 +0530

    Update LadderPresentException.java

--- src/exceptions/LadderPresentException.java
+++ src/exceptions/LadderPresentException.java
@@ -1,6 +1,7 @@
 package exceptions;
 
 public class LadderPresentException extends Exception {
+	// this exception is thrown when a ladder is already present at the location where add snake is called
 	private final static String message = "Ladder Already Present at this Location";
 	public LadderPresentException() {
 		super(message);
