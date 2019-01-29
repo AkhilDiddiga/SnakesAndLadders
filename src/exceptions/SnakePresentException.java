5be6f0d4a5a30e666f93fc6911b05e0c3464e383
AkhilDiddiga <47138605+AkhilDiddiga@users.noreply.github.com>
2019-01-29 17:46:47 +0530

    Update SnakePresentException.java

--- src/exceptions/SnakePresentException.java
+++ src/exceptions/SnakePresentException.java
@@ -1,6 +1,7 @@
 package exceptions;
 
 public class SnakePresentException extends Exception {
+	// this exception is thrown when a snake head is already present at the location where add ladder start is called
 	private final static String message = "Snake Already Present at this Location";
 	public SnakePresentException() {
 		super(message);
