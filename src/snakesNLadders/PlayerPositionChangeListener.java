30557444127d945bcb0fdecf3c7e2eaef5f6b88f
AkhilDiddiga <47138605+AkhilDiddiga@users.noreply.github.com>
2019-01-29 17:54:05 +0530

    Update PlayerPositionChangeListener.java

--- src/snakesNLadders/PlayerPositionChangeListener.java
+++ src/snakesNLadders/PlayerPositionChangeListener.java
@@ -1,5 +1,6 @@
 package snakesNLadders;
 
+// this listener is invoked when player position changes
 public interface PlayerPositionChangeListener {
 	void onPlayerPositionChanged(int oldPos, int newPos);
 }
