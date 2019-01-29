b15a66edad1d2bbd47c33fa2bae929aa6f10219f
AkhilDiddiga <47138605+AkhilDiddiga@users.noreply.github.com>
2019-01-29 17:48:02 +0530

    Update NoPlayersException.java

--- src/exceptions/NoPlayersException.java
+++ src/exceptions/NoPlayersException.java
@@ -1,6 +1,7 @@
 package exceptions;
 
 public class NoPlayersException extends Exception{
+	// this exception is thrown when there are no players to start the game
 	private final static String message = "No Players in the game";
 	public NoPlayersException() {
 		super(message);
