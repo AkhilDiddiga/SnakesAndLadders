e0a6431db0c806c6efc83dfe29865298ce7b344f
AkhilDiddiga <47138605+AkhilDiddiga@users.noreply.github.com>
2019-01-29 17:47:27 +0530

    Update OutOfBoundsException.java

--- src/exceptions/OutOfBoundsException.java
+++ src/exceptions/OutOfBoundsException.java
@@ -1,6 +1,7 @@
 package exceptions;
 
 public class OutOfBoundsException extends Exception{
+	// this exception is thrown when one of the inputs is out of bounds based on the criterion
 	private final static String message = "Input out of Bounds";
 	public OutOfBoundsException() {
 		super(message);
